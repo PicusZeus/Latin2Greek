ec0e83c4a6b3445487f955e82fb8d5a1f16beaad
Mieszko <mieszko.chowaniec@gmail.com>
2014-11-21 14:17:12 +0100

    Zipf!

--- Parser.py
+++ Parser.py
@@ -1,5 +1,16 @@
-'''  Function for initial preparation of the Greek texts from Musaios'''
-def greek_text_rip()
+from collections import Counter
+import re
 
-'''  Function for initial preparation of the Latin texts from Musaios'''
-def latin_text_rip()
+greek_untranslatables = frozenset([u'', ]) 
+
+def try_with_zipfs_law():
+  ''' Use Zipf's Law to identify words with the same counts'''
+  #use counter for greek and latin text
+
+
+def greek_text_rip():
+  '''  Function for initial preparation of the Greek texts from Musaios'''
+
+
+def latin_text_rip():
+  '''  Function for initial preparation of the Latin texts from Musaios'''
