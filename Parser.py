fe44e5e7d01ad2027c313134b60f9da990747499
Mieszko Chowaniec <mchowaniec@newstore.com>
2016-01-12 22:55:07 +0100

    feat(texts) add few first texts

--- Parser.py
+++ Parser.py
@@ -1,16 +1,44 @@
 from collections import Counter
 import re
 
-greek_untranslatables = frozenset([u'', ]) 
+from resource import greek_untranslatables 
 
 def try_with_zipfs_law():
   ''' Use Zipf's Law to identify words with the same counts'''
   #use counter for greek and latin text
 
 
+
 def greek_text_rip():
-  '''  Function for initial preparation of the Greek texts from Musaios'''
+    """ 
+    Function for initial preparation of the Greek texts from Musaios
+    """
+    pass
 
 
 def latin_text_rip():
-  '''  Function for initial preparation of the Latin texts from Musaios'''
+    """
+    Function for initial preparation of the Latin texts from Musaios
+    """
+    pass
+
+
+
+def read_verses(latin_text):
+    """
+    For latin vulgate: read verses
+    """
+    lines = open(latin_text).read().split('\n')
+    for line in lines:
+        chapter_and_verse = line.split(' ')[0]
+        chapter, verse = chapter_and_verse.split()[0], chapter_and_verse.split()[1]
+    return
+
+def clean_clementina(text):
+    """
+    Clean the text of clementina
+    """
+    to_be_replaced = {"æ":"ae","œ":"oe", "Æ": "AE" } #"ë":"?"
+    for key in to_be_replaced.keys():
+        text.replace(key, to_be_replaced[key])
+
--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Citation detection tool for Greek and Latin texts.
 Python 2.7. 
 
 The tool is designed to find quotations, crypto-quotations and allusions from Greek authors in Latin texts and vice versa.
-The principal part of the development involves creating a Latin to Greek dictionary based on existing translations  of ancient texts:
-Eusebius and his version of the Bible, Aratus, Planudes's translations of Vergil and Caesar etc. 
+The principal part of the development involves creating a Latin to Greek dictionary based on existing translations  of ancient texts: LXX/Vulgate,
+Eusebius, Aratus, Planudes's translations of Vergil and Caesar etc. 
